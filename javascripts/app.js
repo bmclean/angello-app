3adc535e247b4d5a20177a5a010fadde86997a1b
Bryce McLean <bmclean@cfactorworks.com>
2014-01-03 15:50:58 -0600

    Adding createStory.

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,1 +1,26 @@
 var myModule = angular.module('Angello', []);
+
+myModule.controller('MainCtrl', function($scope) {
+
+  $scope.currentStory;
+
+  $scope.createStory = function() {
+    $scope.stories.push({
+      title:'New Story',
+      description:'Description pending.'
+    });
+  };
+
+  $scope.setCurrentStory = function(story) {
+    $scope.currentStory = story;
+  };
+
+  $scope.stories = [
+    {title:'Story 00', description:'Description pending.'},
+    {title:'Story 01', description:'Description pending.'},
+    {title:'Story 02', description:'Description pending.'},
+    {title:'Story 03', description:'Description pending.'},
+    {title:'Story 04', description:'Description pending.'},
+    {title:'Story 05', description:'Description pending.'}
+  ];
+});
