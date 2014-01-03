9022a7ac2722a2f9c2e5d92a4ca2008a660513d6
Bryce McLean <bmclean@cfactorworks.com>
2014-01-03 16:27:50 -0600

    Additional fields.

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,13 +1,17 @@
 var myModule = angular.module('Angello', []);
 
 myModule.controller('MainCtrl', function($scope) {
-
   $scope.currentStory;
 
   $scope.createStory = function() {
     $scope.stories.push({
       title:'New Story',
-      description:'Description pending.'
+      description:'Description pending.',
+      criteria:'Criteria pending.',
+      status:'Back Log',
+      type:'Feature',
+      reporter:'Pending',
+      assignee:'Pending'
     });
   };
 
@@ -16,11 +20,11 @@ myModule.controller('MainCtrl', function($scope) {
   };
 
   $scope.stories = [
-    {title:'Story 00', description:'Description pending.'},
-    {title:'Story 01', description:'Description pending.'},
-    {title:'Story 02', description:'Description pending.'},
-    {title:'Story 03', description:'Description pending.'},
-    {title:'Story 04', description:'Description pending.'},
-    {title:'Story 05', description:'Description pending.'}
+    {title:'Story 00', description:'Description pending.', criteria:'Criteria pending.', status:'To Do', type:'Feature', reporter:'Lukas Ruebbelke', assignee:'Brian Ford'},
+    {title:'Story 01', description:'Description pending.', criteria:'Criteria pending.', status:'Back Log', type:'Feature', reporter:'Lukas Ruebbelke', assignee:'Brian Ford'},
+    {title:'Story 02', description:'Description pending.', criteria:'Criteria pending.', status:'Code Review', type:'Enhancement', reporter:'Lukas Ruebbelke', assignee:'Brian Ford'},
+    {title:'Story 03', description:'Description pending.', criteria:'Criteria pending.', status:'Done', type:'Enhancement', reporter:'Lukas Ruebbelke', assignee:'Brian Ford'},
+    {title:'Story 04', description:'Description pending.', criteria:'Criteria pending.', status:'Verified', type:'Bug', reporter:'Lukas Ruebbelke', assignee:'Brian Ford'},
+    {title:'Story 05', description:'Description pending.', criteria:'Criteria pending.', status:'To Do', type:'Spike', reporter:'Lukas Ruebbelke', assignee:'Brian Ford'}
   ];
 });
